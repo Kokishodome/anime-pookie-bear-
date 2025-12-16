d831e11ffa1dccf2082aec58111685f9711aa8cb
slay <159949398+Kokishodome@users.noreply.github.com>
2025-12-16 19:43:56 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let backgroundImgs = document.getElementById('backgroundImgs')
 let rightArrow = document.getElementById('rightArrow')
+let images = ['onePIECE.jpg','DEMONslayerrrrr.png','jjk.jpg']
+let imageNumber = 0
 
 rightArrow.onclick = function () {
+    imageNumber++
     let firstImage = backgroundImgs.children[1]
-    firstImage.style.opacity = 0
-    setTimeout(() => {
-        firstImage.style.transition = '0s'
-        firstImage.style.opacity = 1
-        firstImage.src = 'DEMONslayerrrrr.png'
-    }, 300);
+    firstImage.style.transition = '0.3s !important'
+    setTimeout(() => {   
+        firstImage.style.opacity = 0
+        setTimeout(() => {
+            firstImage.style.transition = '0s'
+            firstImage.style.opacity = 1
+            firstImage.src = images[imageNumber]
+        }, 300);
+    }, 0);
 }
+
+
+// make the search button look normal,
